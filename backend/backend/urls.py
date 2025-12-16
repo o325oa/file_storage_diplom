62514c929245140fb2a69c69c25137004301a154
Никита Роик <roik33@192.168.1.82>
2025-12-16 16:48:46 +0300

    fix settings

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from storage import views
 from rest_framework_simplejwt.views import TokenRefreshView
 from accounts.views import CustomTokenObtainPairView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,3 +14,6 @@ urlpatterns = [
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
