990a902c4a2570d98c4f30c252cc1b699109eabd
Никита Роик <roik33@192.168.1.82>
2025-12-17 15:50:06 +0300

    ff2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/deploy/file_storage_diplom/file_storage_diplom/backend/staticfiles'
 
 FILE_STORAGE_ROOT = os.path.join(BASE_DIR, 'media')
 
