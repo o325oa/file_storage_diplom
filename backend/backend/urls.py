2281fa93a1caa60f5687bf3dd941598f6f76fc6c
Никита Роик <roik33@192.168.1.82>
2025-12-18 11:56:23 +0300

    fix adminpage

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminpage/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/storage/', include('storage.urls')),
     path('s/<str:token>/', views.public_download, name='public_download'),
